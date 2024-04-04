0c965319e6c0f802c76a54425dc811215cd00faf
Siqian <liu00638@algonquinlive.com>
2024-04-04 13:39:36 -0400

    update book.sql

--- db/book.sql
+++ db/book.sql
@@ -1,45 +1,60 @@
-CREATE TABLE users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(50) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL,
-    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-    role ENUM('admin', 'user') DEFAULT 'user'
-);
+SET FOREIGN_KEY_CHECKS=0;
 
-CREATE TABLE books (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(255) NOT NULL,
-    author VARCHAR(255) NOT NULL,
-    isbn VARCHAR(13),
-    publish_date DATE,
-    genre VARCHAR(100),
-    description TEXT,
-    img VARCHAR(255),
-    score DOUBLE,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-);
+-- ----------------------------
+-- Table structure for bookcomments
+-- ----------------------------
+DROP TABLE IF EXISTS `bookcomments`;
+CREATE TABLE `bookcomments` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `bookid` int(11) NOT NULL,
+  `userid` int(11) NOT NULL,
+  `comments` text DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-CREATE TABLE userbooks (
-    userid INT AUTO_INCREMENT PRIMARY KEY,
-    bookid INT PRIMARY KEY,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-);
+-- ----------------------------
+-- Table structure for books
+-- ----------------------------
+DROP TABLE IF EXISTS `books`;
+CREATE TABLE `books` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL,
+  `author` varchar(255) NOT NULL,
+  `isbn` varchar(13) DEFAULT NULL,
+  `publish_date` date DEFAULT NULL,
+  `genre` varchar(100) DEFAULT NULL,
+  `description` text DEFAULT NULL,
+  `img` varchar(255) DEFAULT NULL,
+  `score` double DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2270 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-CREATE TABLE bookcomments (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    bookid int NOT NULL,
-	userid int NOT NULL,
-	comments text,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-);
+-- ----------------------------
+-- Table structure for userbooks
+-- ----------------------------
+DROP TABLE IF EXISTS `userbooks`;
+CREATE TABLE `userbooks` (
+  `userid` int(11) NOT NULL,
+  `bookid` int(11) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  PRIMARY KEY (`userid`,`bookid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-
--- test
---update books set author=SUBSTRING_INDEX(author, ' / ', 1);
---update books set isbn=UPPER(LEFT(MD5(id),10));
---update books set publish_date=DATE_ADD('1970-01-01', INTERVAL FLOOR(RAND() * (DATEDIFF('2023-12-31', '1970-01-01')+1)) DAY);
---update books set genre=ELT(FLOOR(1 + (RAND() * 9)), 'Science', 'Nature', 'Geography','Philosophy','Astronomy','History','Literature','Politics','Economics');
---update books set description=concat('I am description ',id);
+-- ----------------------------
+-- Table structure for users
+-- ----------------------------
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `created_at` datetime DEFAULT current_timestamp(),
+  `role` enum('admin','user') DEFAULT 'user',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
