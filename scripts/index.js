4db216d219e5e98ba98aee59ac9cb46ebd443ad2
Binglei Cai <bingleicai@gmail.com>
2024-04-05 22:54:54 -0400

    Add validation to register form
    1. Add <div> tags in index.html for error messages below each field to output error messages when fields don't meet the requirement
    2. add style for error messages in index.css
    3. add functions in index.js to verify each field and form will only be able to submit when all requirements are met

--- scripts/index.js
+++ scripts/index.js
@@ -37,7 +37,7 @@ document
   .addEventListener("submit", function (e) {
     // prevent default flush page
     e.preventDefault();
-
+    if(validate()) {
     // get data from form
     let jsonData = getJson(new FormData(this));
 
@@ -61,10 +61,72 @@ document
         }
       })
       .catch((err) => console.error("Error: ", err));
-  });
+  }});
 
 function getJson(formData) {
   let obj = {};
   formData.forEach((value, key) => (obj[key] = value));
   return JSON.stringify(obj);
+}
+
+//validate register email
+let registerEmail = document.getElementById('registerEmail');
+let emailerror = document.getElementById('emailerror');
+
+function validateEmail(email) {
+  let emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!emailPattern.test(email.value)) {
+      emailerror.innerText = '* Email address should be non-empty with the format xyz@xyz.xyz.';
+      email.style.border = '2px solid red';
+      return false;
+  } else {
+      emailerror.innerText = '';
+      email.style.border = '1px solid black';
+      return true;
+  }
+}
+
+registerEmail.addEventListener('input', function() {
+  validateEmail(registerEmail);
+})
+
+//validate register password
+let pass = document.getElementById('registerPassword');
+let pass2 = document.getElementById('registerPassword2');
+let passerror = document.getElementById('passerror');
+let pass2error = document.getElementById('pass2error');
+
+function validatePass(password) {
+  if (password.value.length < 8) {
+      passerror.innerText = '* Password should be at least 8 characters long.';
+      password.style.border = '2px solid red';
+      return false;
+  } else {
+      passerror.innerText = '';
+      password.style.border = '1px solid black';
+      return true;
+  }
+}
+
+function validatePass2(password1, password2) {
+  if (password1.value !== password2.value) {
+      pass2error.innerText = '* Please retype password.';
+      pass2.style.border = '2px solid red';
+      return false;
+  } else {
+      pass2error.innerText = '';
+      password2.style.border = '1px solid black';
+      return true;
+  }
+}
+pass.addEventListener('input', function() {
+  validatePass(pass);
+})
+
+pass2.addEventListener('input', function() {
+  validatePass2(pass, pass2);
+})
+
+function validate() {
+  return validateEmail(registerEmail) && validatePass(pass) && validatePass2(pass, pass2);
 }
